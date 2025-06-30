8c3779e452998c1d59e521a2be4f730d413851a5
Aaron Allred <125822991+aballred351@users.noreply.github.com>
2025-06-30 15:14:44 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.redirect('/games'));
+app.get('/admin', (req, res) => res.sendFile(__dirname + '/views/admin.html'));
+app.get('/games', (req, res) => res.sendFile(__dirname + '/views/games.html'));
+
+app.post('/submit', (req, res) => {
+  const newGame = req.body;
+  const filePath = './data/games.json';
+
+  const games = JSON.parse(fs.readFileSync(filePath));
+  games.push(newGame);
+  fs.writeFileSync(filePath, JSON.stringify(games, null, 2));
+
+  res.redirect('/games');
+});
+
+app.get('/api/games', (req, res) => {
+  const games = JSON.parse(fs.readFileSync('./data/games.json'));
+  res.json(games);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
